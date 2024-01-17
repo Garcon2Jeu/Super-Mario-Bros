89b9347772c13744a7e933b9a551ca9d01b4c24a
Garcon2Jeu <104232891+Garcon2Jeu@users.noreply.github.com>
2024-01-17 15:27:48 +0100

    Fixed PlayerRunState's bug

--- modules/MapPointerModule.lua
+++ modules/MapPointerModule.lua
@@ -1,12 +1,10 @@
 MapPointerModule = Class()
 
 function MapPointerModule:pointToTile(map, x, y)
-    if x < 0
-        or y < 0
-        -- Ugly?--
-        or x > State.current.level.width
-        or y > State.current.level.height then
-        -- Ugly?--
+    if x < 0 or y < 0
+        -- Ugly?!--
+        or x > State.current.level.width or y > State.current.level.height then
+        -- Ugly?!--
         return nil
     end
 
@@ -20,9 +18,7 @@ function MapPointerModule:getTilesFromHitPoints(map, object, edgeDirection)
     local x1, y1, x2, y2 = object:getHitboxOffset(edgeDirection)
     local p1, p2 = object:getHitboxEdge(edgeDirection, x1, y1, x2, y2)
 
-    return self:pointToTile(map,
-            p1.x,
-            p1.y),
+    return self:pointToTile(map, p1.x, p1.y),
         self:pointToTile(map, p2.x, p2.y)
 end
 
--- states/player/PlayerRunState.lua
+++ states/player/PlayerRunState.lua
@@ -17,39 +17,7 @@ function PlayerRunState:update(dt)
         return
     end
 
-    local t1, t2 = self.avatar:getTilesFromHitPoints(self.level.tileMap, self.avatar, "bottom")
-    local b1, b2 = self.avatar:getTilesFromHitPoints(self.level.blockMap, self.avatar, "bottom")
-
-    -- TOFIX!!!!!! ----------------------------------------------------------------------------------------------------------------------
-
-    -- for key, object in pairs { t1, t2, b1, b2 } do
-    --     if not self.level:isTileCollidable(object) then
-    --         self.avatar:changeState("fall")
-    --         return
-    --     end
-    -- end
-
-    -- if not self.level:isTileCollidable(t1) and not self.level:isTileCollidable(t2)
-    --     or not self.level:isTileCollidable(b1) and not self.level:isTileCollidable(b2) then
-    --     self.avatar:changeState("fall")
-    --     return
-    -- end
-
-    if not self.avatar:isTileCollidable(t1) and not self.avatar:isTileCollidable(t2) then
-        self.avatar:changeState("fall")
-        return
-    end
-    -- if not self.level:isTileCollidable(b1) and not self.level:isTileCollidable(b2) then
-    --     self.avatar:changeState("fall")
-    --     return
-    -- end
-
-    -- if not self.level:isTileCollidable(b1) and not self.level:isTileCollidable(b2) then
-    --     self.avatar:changeState("fall")
-    --     return
-    -- end
-
-    ------------------------------------------------------------------------------------------------------------------------------------
+    self:checkForGround()
 
     if not self.avatar:isRunning() then
         self.avatar:changeState("idle")
@@ -59,4 +27,19 @@ end
 
 function PlayerRunState:exit()
     self:removeTimer(self)
+end
+
+function PlayerRunState:checkForGround()
+    local t1, t2 = self.avatar:getTilesFromHitPoints(
+        self.level.tileMap, self.avatar, "bottom")
+    local b1, b2 = self.avatar:getTilesFromHitPoints(
+        self.level.blockMap, self.avatar, "bottom")
+
+    for key, tile in pairs { t1, t2, b1, b2 } do
+        if self.avatar:isTileCollidable(tile) then
+            return
+        end
+    end
+
+    self.avatar:changeState("fall")
 end
